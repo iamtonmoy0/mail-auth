50d442859d23b6da9ec29d83935411c5713c0280
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-18 18:22:42 -0400

    feat : empty validation added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,10 @@
 import  {getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import app from '../../Firebase/Firebase.config';
+import { useState } from 'react';
 const auth=getAuth(app)
 const Register = () => {
-	
+	const[error,setError]=useState('')
+	const[success,setSuccess]=useState('')
 
 	const handleSubmit=(e)=>{
 		e.preventDefault();
@@ -13,10 +15,15 @@ const Register = () => {
 		.then(userCredential=>{
 			const loggedUser=userCredential.user;
 			console.log(loggedUser)
+			e.target.reset();
+			setError('')
+			setSuccess('user registered success')
 			
 		})
 		.catch(error=>{
-			console.log(error)
+		
+			setError(error.message)
+			setSuccess('')
 		})
               
 	}
@@ -27,12 +34,14 @@ const Register = () => {
 			<form onSubmit={handleSubmit}>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="email1" aria-describedby="emailHelp" name="email"/>
+    <input type="email" className="form-control" id="email1" aria-describedby="emailHelp" name="email" required/>
  
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="password" name="password /"/>
+    <input type="password" className="form-control" id="password" name="password" required/>
+    <p>{error}</p>
+    <p>{success}</p>
   </div>
   <div className="mb-3 form-check">
     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
